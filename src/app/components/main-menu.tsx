47f354f3f64808c075e59f8d40aa196217a4cb31
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-05 03:54:26 -0600

    changed structure of menu items

--- src/app/components/main-menu.tsx
+++ src/app/components/main-menu.tsx
@@ -1,16 +1,32 @@
-import { Link } from "@nextui-org/link"
-import { css } from "@emotion/react"
+'use server'
+import { Link } from "@nextui-org/react"
 import React from "react";
 import { IoInformationCircleSharp, IoConstruct, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
-import { IconContext } from "react-icons/lib/esm/iconContext";
 
 const MainMenu: React.FC = () => {
     const menuItems = [
-        <AboutComponent key='menu-about-item' />,
-        <ProjectsComponent key='menu-projects-item' />,
-        <LinkedInComponent key='menu-linkedin-item' />,
-        <GithubComponent key='menu-github-item' />
+        {
+            icon: <IoInformationCircleSharp className='h-full text-default-900  self-center' />,
+            link: '/about',
+            text: 'About'
+        },
+        {
+            icon: <IoConstruct className='h-full text-default-900 self-center' />,
+            link: '/projects',
+            text: 'Projects'
+        },
+        {
+            icon: <IoLogoLinkedin className='h-full text-default-900 self-center' />,
+            link: 'https://www.linkedin.com/in/chris-billingham-7781a1180/',
+            text: 'LinkedIn'
+        },
+        {
+            icon: <IoLogoGithub className='h-full text-default-900 self-center' />,
+            link: 'https://www.github.com/ChrisIBill',
+            text: 'Github'
+        },
     ]
+
     return (
         <div className="relative flex flex-col w-full min-h-fit items-center justify-center transition-transform animate-slideInRight">
             <ul className="relative top-8 flex flex-col w-fit h-fit">
@@ -18,10 +34,17 @@ const MainMenu: React.FC = () => {
                     menuItems.map((item, index) => {
                         return (
                             <li
-                                key={item.key}
+                                key={item.text + '-menu-item'}
                                 className='flex'
+                            //TODO: why doesnt this work to add margin to icons?
+                            //css={css`
+                            //&:nth-child(1) {
+                            //    margin-left: 8rem;
+                            //}
+                            //`}
                             >
-                                {item}
+                                {item.icon}
+                                <Link className='ml-2' href={item.link}>{item.text}</Link>
                             </li>
                         )
                     })
@@ -30,39 +53,5 @@ const MainMenu: React.FC = () => {
         </div>
     )
 }
-
-const AboutComponent: React.FC = () => {
-    return (
-        <React.Fragment>
-            <IoInformationCircleSharp className='text-default-800 h-full self-center' />
-            <Link href='/about'>About</Link>
-        </React.Fragment>
-    )
-}
-const ProjectsComponent: React.FC = () => {
-    return (
-        <React.Fragment>
-            <IoConstruct className='text-default-800 h-full self-center' />
-            <Link href='/projects'>Projects</Link>
-        </React.Fragment>
-    )
-}
-const LinkedInComponent: React.FC = () => {
-    return (
-        <React.Fragment>
-            <IoLogoLinkedin className='text-default-800 h-full self-center' />
-            <Link href='https://www.linkedin.com/in/chris-billingham-7781a1180/'>LinkedIn</Link>
-        </React.Fragment>
-    )
-}
-const GithubComponent: React.FC = () => {
-    return (
-        <React.Fragment>
-            <IoLogoGithub className='text-default-800 h-full self-center' />
-            <Link href='https://github.com/ChrisIBill'>Github</Link>
-        </React.Fragment>
-    )
-}
-
 
 export default MainMenu
