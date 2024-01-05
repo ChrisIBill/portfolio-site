2082020654cb36bba76ab7dbb69cdb602c4f1491
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-05 01:35:48 -0600

    bio and main menu styling work

--- src/app/components/bio.tsx
+++ src/app/components/bio.tsx
@@ -1,15 +1,18 @@
 const BioComponent: React.FC = () => {
     return (
-        <div className="flex flex-col justify-center translate-x-full transition">
-            <h1 className="text-lg my-3 text-foreground-900">
-                Christopher Billingham
-            </h1>
-            <span className="text-foreground-600">
-                B.S. Computer Science, University of Texas at Dallas
-            </span>
-            <span className="text-foreground-600">
-                Software Engineer, Web Developer
-            </span>
+        <div className="relative flex flex-col w-full min-h-fit items-center justify-center transition-transform animate-slideInLeft">
+            <div className="relative bottom-8 flex flex-col w-fit justify-center">
+                <h1 className="text-3xl my-3 text-foreground-900">
+                    Christopher Billingham
+                </h1>
+                <span className="italic text-foreground-600">
+                    B.S. Computer Science, University of Texas at Dallas
+                </span>
+                <span className="italic text-foreground-600">
+                    Software Engineer, Web Developer
+                </span>
+                <hr className="bg-gradient-to-r from-default-900 to-default-900/0 w-7/8 my-3 border-0 h-px" />
+            </div>
         </div>
     )
 }
--- src/app/components/main-menu.tsx
+++ src/app/components/main-menu.tsx
@@ -1,1 +1,68 @@
+import { Link } from "@nextui-org/link"
+import { css } from "@emotion/react"
+import React from "react";
+import { IoInformationCircleSharp, IoConstruct, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
+import { IconContext } from "react-icons/lib/esm/iconContext";
+
+const MainMenu: React.FC = () => {
+    const menuItems = [
+        <AboutComponent key='menu-about-item' />,
+        <ProjectsComponent key='menu-projects-item' />,
+        <LinkedInComponent key='menu-linkedin-item' />,
+        <GithubComponent key='menu-github-item' />
+    ]
+    return (
+        <div className="relative flex flex-col w-full min-h-fit items-center justify-center transition-transform animate-slideInRight">
             <ul className="relative top-8 flex flex-col w-fit h-fit">
+                {
+                    menuItems.map((item, index) => {
+                        return (
+                            <li
+                                key={item.key}
+                                className='flex'
+                            >
+                                {item}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+const AboutComponent: React.FC = () => {
+    return (
+        <React.Fragment>
+            <IoInformationCircleSharp className='text-default-800 h-full self-center' />
+            <Link href='/about'>About</Link>
+        </React.Fragment>
+    )
+}
+const ProjectsComponent: React.FC = () => {
+    return (
+        <React.Fragment>
+            <IoConstruct className='text-default-800 h-full self-center' />
+            <Link href='/projects'>Projects</Link>
+        </React.Fragment>
+    )
+}
+const LinkedInComponent: React.FC = () => {
+    return (
+        <React.Fragment>
+            <IoLogoLinkedin className='text-default-800 h-full self-center' />
+            <Link href='https://www.linkedin.com/in/chris-billingham-7781a1180/'>LinkedIn</Link>
+        </React.Fragment>
+    )
+}
+const GithubComponent: React.FC = () => {
+    return (
+        <React.Fragment>
+            <IoLogoGithub className='text-default-800 h-full self-center' />
+            <Link href='https://github.com/ChrisIBill'>Github</Link>
+        </React.Fragment>
+    )
+}
+
+
+export default MainMenu
