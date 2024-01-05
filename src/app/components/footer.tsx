f5a9c38f22bfe35d0f9d82c9a6fce3703ac7de0d
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-05 07:30:06 -0600

    improved footer styling

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer className="absolute bottom-0">
             <div className="flex w-screen justify-center">
-                <span className="text-default-500 whitespace-normal mr-1">Made with</span>
-                <Link href="https://nextjs.org/">
+                <span className="text-default-500 text-sm whitespace-normal mr-1">Made with</span>
+                <Link href="https://nextjs.org/" className='text-sm'>
                     Next.JS
                 </Link>
             </div>
