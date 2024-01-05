5c12814b9eb1ca0b1670761e1fb6bcf9611ab26d
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-05 03:54:59 -0600

    bio styling work

--- src/app/components/bio.tsx
+++ src/app/components/bio.tsx
@@ -2,7 +2,7 @@ const BioComponent: React.FC = () => {
     return (
         <div className="relative flex flex-col w-full min-h-fit items-center justify-center transition-transform animate-slideInLeft">
             <div className="relative bottom-8 flex flex-col w-fit justify-center">
-                <h1 className="text-3xl my-3 text-foreground-900">
+                <h1 className="text-3xl my-3 text-foreground-900 self-center">
                     Christopher Billingham
                 </h1>
                 <span className="italic text-foreground-600">
@@ -11,7 +11,7 @@ const BioComponent: React.FC = () => {
                 <span className="italic text-foreground-600">
                     Software Engineer, Web Developer
                 </span>
-                <hr className="bg-gradient-to-r from-default-900 to-default-900/0 w-7/8 my-3 border-0 h-px" />
+                <hr className="bg-gradient-to-r from-default-900/0 via-default-900 to-default-900/0 w-7/8 my-3 border-0 h-px" />
             </div>
         </div>
     )
