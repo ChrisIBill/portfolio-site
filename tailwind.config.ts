1d5768936ab23dd0865937ffc09003ddb5b115d1
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-04 23:40:17 -0600

    added new slideIn animations to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,25 @@ const config: Config = {
     ],
     theme: {
         extend: {
+            keyframes: {
+                slideInLeft: {
+                    '0%': { transform: 'translateX(-150%)' },
+                    '100%': { transform: 'translateX(0)' },
+                },
+                slideInRight: {
+                    '0%': { transform: 'translateX(150%)' },
+                    '100%': { transform: 'translateX(0)' },
+                },
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-3deg)' },
+                    '50%': { transform: 'rotate(3deg)' },
+                },
+            },
+            animation: {
+                slideInLeft: 'slideInLeft 1s ease-in-out',
+                slideInRight: 'slideInRight 1s ease-in-out',
+                wiggle: 'wiggle 1s ease-in-out infinite',
+            },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
