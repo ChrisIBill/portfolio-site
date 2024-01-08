f24095ac5e8e1eb4301e14c2bd44230326db8aae
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-08 15:31:24 -0600

    fixed theme color for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,15 @@
 import { Link } from '@nextui-org/react'
 
+export async function getStaticProps(context) {
+    return {
+        notFound: true, // triggers 404
+    };
+}
+
 export default function NotFound() {
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center'>
-            <h2 className='text-3xl m-4'>404: Page Not Found</h2>
+            <h2 className='text-3xl m-4 text-default-900'>404: Page Not Found</h2>
             <Link href="/">Return Home</Link>
         </div>
     )
