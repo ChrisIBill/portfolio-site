c5d406a266699fffff86ab4f3133e69a95ffcbbc
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-08 20:29:33 -0600

    added className prop to providers component

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -6,11 +6,11 @@ import { ThemeProvider } from 'next-themes';
 import { useRouter } from 'next/navigation'
 import { IconContext } from 'react-icons';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children, className }: { children: React.ReactNode, className: string }) {
     const router = useRouter();
 
     return (
-        <NextUIProvider navigate={router.push}>
+        <NextUIProvider className={className} navigate={router.push}>
             <ThemeProvider defaultTheme='dark'>
                 <IconContext.Provider value={{ color: 'blue', className: 'text-black' }}>
                     <RootStyleRegistry>
