0c14617951fbd7fc9a1bd0e3053fc2040156613a
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-09 22:57:36 -0600

    added external link handling to site links

--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -1,5 +1,12 @@
 import { IoConstruct, IoHome, IoInformationCircleSharp, IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 
+export interface SiteLinkType {
+    icon: React.ReactNode,
+    link: string,
+    text: string,
+    label: string
+    isExternal?: boolean
+}
 export const SiteLinks = [
     {
         icon: <IoHome className='h-full text-default-900 self-center' />,
@@ -23,12 +30,14 @@ export const SiteLinks = [
         icon: <IoLogoLinkedin className='h-full text-default-900 self-center' />,
         link: 'https://www.linkedin.com/in/chris-billingham-7781a1180/',
         text: 'LinkedIn',
-        label: 'Link to my LinkedIn'
+        label: 'Link to my LinkedIn',
+        isExternal: true,
     },
     {
         icon: <IoLogoGithub className='h-full text-default-900 self-center' />,
         link: 'https://www.github.com/ChrisIBill',
         text: 'Github',
-        label: 'Link to my Github'
+        label: 'Link to my Github',
+        isExternal: true,
     },
 ] as const
