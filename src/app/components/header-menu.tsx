32b2b9ef54538db6385d4bdf89400bb9c4697094
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-30 02:22:18 -0600

    cleanup and moved navbar menu to own file, switched navbar menu to dynamic component

---
+++ src/app/components/header-menu.tsx
@@ -0,0 +1,79 @@
+import { SiteLinks, SiteLinkType } from "./links"
+import { NavbarContent, Link, NavbarItem, NavbarMenu, NavbarMenuItem } from "@nextui-org/react"
+import React from "react"
+import { useIsLessThanXS } from "@/lib/hooks/resize"
+
+const NavBarMenu = (props: {
+    pathname: string
+}) => {
+    const lessThanXs = useIsLessThanXS()
+    const [isClient, setIsClient] = React.useState(false)
+
+    React.useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    if (!isClient)
+        return <ListMenuItems pathname={props.pathname} />
+    return lessThanXs ? <ListMenuItems pathname={props.pathname} />
+        : <ContentMenuItems pathname={props.pathname} />
+}
+
+const ListMenuItems = (props: any) => {
+    return (
+        <NavbarMenu className='flex flex-col gap-4'>
+            {SiteLinks.map((item) => {
+                return (
+                    <NavbarMenuItem
+                        isActive={item.link === props.pathname}
+                        key={item.text + '-menu-item'}
+                        className='flex nowrap items-center'
+                    >
+                        <MenuItemLink item={item} pathname={props.pathname} />
+                    </NavbarMenuItem>
+                )
+            })}
+        </NavbarMenu>
+    )
+}
+
+const ContentMenuItems = (props: any) => {
+    return (
+        <NavbarContent className='flex gap-4' justify="center">
+            {SiteLinks.map((item) => {
+                return (
+                    <NavbarItem
+                        isActive={item.link === props.pathname}
+                        key={item.text + '-menu-item'}
+                        className='flex nowrap items-center'
+                    >
+                        <MenuItemLink item={item} pathname={props.pathname} />
+                    </NavbarItem>
+                )
+            })}
+        </NavbarContent >
+    )
+}
+const MenuItemLink = (props: {
+    item: SiteLinkType
+    pathname: string
+}) => {
+    return (
+        <Link
+            aria-label={props.item.text + ' menu item'}
+            title={props.item.label}
+            className='ml-2 text-inherit'
+            href={props.item.link}
+            isExternal={props.item.isExternal}
+        >
+            {props.item.icon}
+            <div
+                className={`${props.item.link === props.pathname ? '' : 'inline xs:hidden sm:inline'} ml-2 sm:flex text-inherit`}
+            >
+                {props.item.text}
+            </div>
+        </Link>
+    )
+}
+
+export default NavBarMenu
