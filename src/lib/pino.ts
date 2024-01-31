eefc0e7b1058a39ad28051b238f2a710d7ccbb1c
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-01-30 23:45:05 -0600

    pino fixes

--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -5,32 +5,37 @@ const config = {
     env: process.env.NODE_ENV,
     publicUrl: process.env.PUBLIC_URL,
 }
+const send = (level: any, logEvent: { messages: any[] }) => {
+    const msg = logEvent.messages[0]
+
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        type: 'application/json'
+    }
+    let blob = new Blob([JSON.stringify({ msg, level })], headers)
+    navigator.sendBeacon(`${config.serverUrl}/log`, blob)
+}
 
 const pinoConfig = {
     browser: {
         asObject: true,
         serialize: true,
+        transmit: {
+            level: 'info',
+            send: send
+        }
     }
 }
 
 if (config.serverUrl) {
     pinoConfig.browser.transmit = {
         level: 'info',
-        send: (level, logEvent) => {
-            const msg = logEvent.messages[0]
-
-            const headers = {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-                type: 'application/json'
-            }
-            let blob = new Blob([JSON.stringify({ msg, level })], headers)
-            navigator.sendBeacon(`${config.serverUrl}/log`, blob)
-        }
+        send: send
     }
 }
 
 const logger = pino(pinoConfig)
 
-export const log = msg => logger.info(msg)
+export const log = (msg: any) => logger.info(msg)
 export default logger
