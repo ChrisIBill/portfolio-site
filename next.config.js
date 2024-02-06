6655f94a5fb882efa57ec28609c42756b5c466a7
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-02-05 18:29:04 -0600

    added robots index header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'index, follow',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
