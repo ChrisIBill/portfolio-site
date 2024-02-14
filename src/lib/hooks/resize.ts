f921e2922bd1286e75d606152d4722cbf5b3bf24
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-02-13 18:57:16 -0600

    resize hook work

--- src/lib/hooks/resize.ts
+++ src/lib/hooks/resize.ts
@@ -1,22 +1,31 @@
+'use client'
 import { useEffect, useRef, useState } from "react"
 import { EXTRA_SMALL } from "../constants"
+import logger from "../pino"
+
+const WindowResizeLog = logger.child({ module: "WindowResize" })
 
 const useWindowResizeThreshold = (threshold: number) => {
     const [isThresholdSize, setIsThresholdSize] = useState(true)
     const prevWidth = useRef(0)
     const [isMounted, setIsMounted] = useState(false)
+    WindowResizeLog.debug({ message: 'hook called', threshold, isThresholdSize, isMounted })
 
     useEffect(() => {
         if (!isMounted) {
+            WindowResizeLog.debug({ message: 'mounted' })
             setIsMounted(true)
             prevWidth.current = window.innerWidth
             setIsThresholdSize(prevWidth.current <= threshold)
         }
         const handleResize = () => {
             const currWidth = window.innerWidth
+            WindowResizeLog.debug({ message: 'resize event', currWidth, prevWidth: prevWidth.current, threshold, isThresholdSize })
             if (currWidth <= threshold && prevWidth.current > threshold) {
+                WindowResizeLog.debug({ message: 'resized to below threshold', currWidth, prevWidth: prevWidth.current, threshold, isThresholdSize })
                 setIsThresholdSize(true)
             } else if (currWidth > threshold && prevWidth.current <= threshold) {
+                WindowResizeLog.debug({ message: 'resized to above threshold', currWidth, prevWidth: prevWidth.current, threshold, isThresholdSize })
                 setIsThresholdSize(false)
             }
             prevWidth.current = currWidth
