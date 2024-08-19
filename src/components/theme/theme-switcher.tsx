313d0ac846f0991c0894c5f5a42db4e95363bade
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-08-19 15:09:09 -0500

    theme-switcher update fixes

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -1,47 +1,56 @@
-'use client'
+"use client";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import "@theme-toggles/react/css/Classic.css"
+import "@theme-toggles/react/css/Classic.css";
 
 import { Classic } from "@theme-toggles/react";
 import logger from "@/lib/pino";
 
-type ThemeType = "dark" | "light"
-const ThemeLogger = logger.child({ module: 'Theme' })
+type ThemeType = "dark" | "light";
+const ThemeLogger = logger.child({ module: "Theme" });
 const ThemeSwitcher = () => {
-    const [mounted, setMounted] = useState(false)
-    const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-    const handleThemeChange = () => {
-        const newTheme = theme === "dark" ? "light" : "dark"
-        ThemeLogger.info('theme changed: ' + newTheme)
-        setTheme(newTheme)
-        localStorage.setItem('theme', newTheme)
-        document.documentElement.classList.toggle('dark')
+  const handleThemeChange = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    ThemeLogger.info("theme changed: " + newTheme);
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
     }
+    setMounted(true);
+  }, []);
 
-    useEffect(() => {
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            setTheme('dark')
-            document.documentElement.classList.add('dark')
-        } else {
-            setTheme('light')
-            document.documentElement.classList.remove('dark')
-        }
-        setMounted(true)
-    }, [])
-
-    if (!mounted) {
-        return null
-    }
-    return (
-        <div
-            className='text-3xl w-4 mx-4 mt-2 text-foreground'
-        >
-            <Classic duration={750} placeholder={<></>} toggled={theme === "dark"} onToggle={handleThemeChange} forceMotion />
-        </div>
-    )
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <div className="text-3xl w-4 mx-4 mt-2 text-foreground">
+      <Classic
+        onPointerLeaveCapture={() => {}}
+        onPointerEnterCapture={() => {}}
+        duration={750}
+        placeholder={<></>}
+        toggled={theme === "dark"}
+        onToggle={handleThemeChange}
+        forceMotion
+      />
+    </div>
+  );
 };
-
 
 export default ThemeSwitcher;
